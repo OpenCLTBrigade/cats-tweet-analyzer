a0b9e2d86565075d24186296f41240c64f55f71e
Andrew Natoli <4162073+AndrewNatoli@users.noreply.github.com>
2019-10-21 16:40:15 -0400

    feat: Introduce a method for setting credentials (setConfig()) that doesn't box you into tricky dotenv situations. Roll your own way of pulling credentials into your app (dotenv / secrets managers like SSM / etc), then call `setConfig` to set the configuration that the Twitter client will reference.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 import analyzeTweets from './analyzeTweets';
+import { getConfig, setConfig } from './config';
 import getTimeline from './getTimeline';
 import getTwitterClient from './getTwitterClient';
 
-export { getTwitterClient, getTimeline, analyzeTweets };
+export { getTwitterClient, getTimeline, analyzeTweets, getConfig, setConfig };
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,9 @@
 import AnalyzedTweet from './AnalyzedTweet';
+import Configuration from './Configuration';
 import NestedTweet from './NestedTweet';
 import ServiceDelayData from './ServiceDelayData';
 import ServiceRestorationData from './ServiceRestorationData';
 import TimelineResponse from './TimelineResponse';
 import Tweet from './Tweet';
 
-export { Tweet, TimelineResponse, AnalyzedTweet, ServiceDelayData, ServiceRestorationData, NestedTweet };
+export { Tweet, TimelineResponse, AnalyzedTweet, ServiceDelayData, ServiceRestorationData, NestedTweet, Configuration };
